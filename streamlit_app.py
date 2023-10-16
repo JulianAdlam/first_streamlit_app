2ce8a2f9e60d863fbc15135cf01935ea085099fc
JulianAdlam <julian.adlam@gmail.com>
2023-10-16 21:23:24 +0100

    Update streamlit_app.py
    Changed Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mam's New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
